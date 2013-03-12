83b998876e3537afef10dfae0c095005d4a0c518
Jamey DeOrio <jameyd@darkhorse.com>
2013-03-12 10:37:37 -0700

    Fix create_symlinks script (root was not an abspath)

--- create_symlinks.py
+++ create_symlinks.py
@@ -9,7 +9,7 @@ for root, dirs, files in os.walk('.'):
     for filename in files:
         if filename.endswith('.symlink'):
             new_file = home + '/.' + filename[:-8]
-            file_path = root + '/' + filename
+            file_path = os.path.abspath(root) + '/' + filename
             if os.path.isfile(new_file):
                 print "Skipped existing symlink to {0} @ {1}!".format(file_path, new_file)
             else:
