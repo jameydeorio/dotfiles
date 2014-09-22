b55f27da52d8d8d1a3ff8a17bb3723162215657c
Jamey J. DeOrio <jameyd@darkhorse.com>
2014-09-21 22:18:57 -0700

    Add --force option to create_symlinks

--- create_symlinks.py
+++ create_symlinks.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
 
 import os
+import argparse
 
+parser = argparse.ArgumentParser(description='Create symlinks to dotfiles')
+parser.add_argument('-f', '--force', action='store_true', help='override existing symlinks')
+args = parser.parse_args()
 
 home = os.path.expanduser('~')
 
@@ -11,8 +15,12 @@ for root, dirs, files in os.walk('.'):
             new_file = home + '/.' + filename[:-8]
             file_path = os.path.abspath(root) + '/' + filename
             if os.path.isfile(new_file):
-                print "Skipped existing symlink to {0} @ {1}!".format(file_path, new_file)
+                if args.force:
+                    os.unlink(new_file)
+                    os.symlink(file_path, new_file)
+                    print "Overrode symlink @ {} with {}!".format(new_file, file_path)
+                else:
+                    print "Skipped existing symlink to {} @ {}!".format(file_path, new_file)
             else:
                 os.symlink(file_path, new_file)
                 print "Created symlink to {0} @ {1}!".format(file_path, new_file)
-
